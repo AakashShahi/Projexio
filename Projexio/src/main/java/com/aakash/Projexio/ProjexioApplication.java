252d91e5bc76053f99d32795786ca47d3b00c0d6
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-08-17 16:55:28 +0545

    Jwwt config

---
+++ Projexio/src/main/java/com/aakash/Projexio/ProjexioApplication.java
@@ -0,0 +1,13 @@
+package com.aakash.Projexio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjexioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjexioApplication.class, args);
+	}
+
+}
---
+++ Projexio/src/main/java/com/aakash/Projexio/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.aakash.Projexio.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "secretkeybhbbhgvhgcfcghbcwaqaytboi";
+    public static final String JWT_HEADER = "Authorization";
+
+}
